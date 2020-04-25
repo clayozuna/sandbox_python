d3d211775fd94ddf83c2150417eb03edfb8baf08
Clay Ozuna <ozunaclay@gmail.com>
2020-04-25 01:00:28 -0500

    remove API key

--- send_sms.py
+++ send_sms.py
@@ -3,7 +3,7 @@
 from twilio.rest import Client
 
 # the following line needs your Twilio Account SID and Auth Token
-client = Client("AC12114d20f8f75946a620c74099bd363a", "026d66a6d5b6954334ec4a1043c1163c")
+client = Client("", "026d66a6d5b6954334ec4a1043c1163c")
 
 # change the "from_" number to your Twilio number and the "to" number
 # to the phone number you signed up for Twilio with, or upgrade your
